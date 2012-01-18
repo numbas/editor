51ef811cd6ff133d89a16f16f857ccb95e0a545f
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-01-18 16:25:38 +0000

    Bind model to form, force GET/POST on detail view

--- editor/views/exams.py
+++ editor/views/exams.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from editor.models import Exam, ExamForm
 
+@require_http_methods(["GET", "POST"])
 def detail(request, exam_id):
     e = get_object_or_404(Exam, pk=exam_id)
-    form = ExamForm()
+    form = ExamForm(instance=e)
     return render(request, 'exams/detail.html', {'exam': e, 'form': form})

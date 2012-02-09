c0c044ca4a0d50e6415a31b192cb6332d698f224
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-02-09 16:08:51 +0000

    Added file for admin generator

---
+++ editor/admin.py
@@ -0,0 +1,6 @@
+from editor.models import Exam, ExamQuestion, Question
+from django.contrib import admin
+
+admin.site.register(Exam)
+admin.site.register(ExamQuestion)
+admin.site.register(Question)

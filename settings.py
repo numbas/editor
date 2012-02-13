7f3996120d083e7d0bff3309ab6c4380a1625454
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-02-13 16:20:22 +0000

    Now using MySQL

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/space/najy2/django/numbas/numbas/numbas.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'numbas',                      # Or path to database file if using sqlite3.
+        'USER': 'numbas',                      # Not used with sqlite3.
+        'PASSWORD': 'numb4s',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }

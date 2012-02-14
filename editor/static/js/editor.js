0cf4a665238b58f9a095560748c7abc8e5b84996
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-02-14 14:09:36 +0000

    Removed console.log

--- editor/static/js/editor.js
+++ editor/static/js/editor.js
@@ -1,8 +1,6 @@
 (function() {
 	$(document).ready(function() {
 		$('#exam-preview').click(function() {
-//			$('#some-stuff').load('{% url exam_ajaxtest %}');
-//			$('#some-stuff').load(
 			$.post(
 				Editor.exam_preview_url,
 				{
@@ -17,7 +15,6 @@
 			})
 			.error(function(response, status, xhr) {
 				$('#exam-preview-message').html(response.responseText);
-				console.log(response.responseText);
 			});
 		});
 	});

d7d9a6179251a1c888bc3d89105ba8fa654a43c3
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-02-22 11:52:27 +0000

    Form "content" fields now contain full object
    Using ExamParser to extract name field from content, so that it can be
    inserted into the database.
    
    This has broken the preview code, temporarily.

--- editor/views/generic.py
+++ editor/views/generic.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from examparser import ExamParser
+
 class SaveContentMixin():
     
     """Save exam or question content to a git repository and to a database."""
@@ -14,7 +16,10 @@ class SaveContentMixin():
 #    template_name = None
     
     def write_content(self, directory, form, inlines=None):
+        parser = ExamParser()
         try:
+            content = parser.parse(self.object.content)
+            self.object.name = content['name']
             repo = git.Repo(settings.GLOBAL_SETTINGS['REPO_PATH'])
             os.environ['GIT_AUTHOR_NAME'] = 'Numbas'
             os.environ['GIT_AUTHOR_EMAIL'] = 'numbas@ncl.ac.uk'
@@ -25,8 +30,8 @@ class SaveContentMixin():
             fh.close()
             repo.index.add([os.path.join(directory, self.object.filename)])
             repo.index.commit('Made some changes to %s' % self.object.name)
-        except (IOError, OSError):
-            error = "Could not save file."
+        except (IOError, OSError, IndexError) as err:
+            error = 'Error: ' + str(err)
             return render(self.request, self.template_name,
                           {'form': form, 'inlines': inlines, 'error': error,
                            'object': self.object})

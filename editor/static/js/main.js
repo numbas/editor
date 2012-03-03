07f0f85ba03a7bab600b8f4bf3f817940daac0da
Christian Perfect <christianperfect@gmail.com>
2012-03-03 10:32:04 +0000

    add main.js - functions used everywhere

---
+++ editor/static/js/main.js
@@ -0,0 +1,10 @@
+//nice short 'string contains' function
+if(!String.prototype.contains)
+{
+	String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+}
+if(!Array.prototype.contains)
+{
+	Array.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+}
+

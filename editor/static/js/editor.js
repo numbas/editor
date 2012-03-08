387d4c0397499cbac18f0c24f7385a52b72adc25
Christian Perfect <christianperfect@gmail.com>
2012-03-08 22:33:58 +0000

    remove getCookie from editor.js

--- editor/static/js/editor.js
+++ editor/static/js/editor.js
@@ -19,22 +19,6 @@ if(!window.Editor)
 
 $(document).ready(function() {
 
-    Editor.getCookie = function(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
     slugify = function(s) {
         return s.trim().replace(/[^\w\s]/g,'').toLowerCase().replace(/\s/g,'-');
     };

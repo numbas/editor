9eb19445a91c56297f2799bdeaa527a6ef358c94
Anthony Youd <Anthony.Youd@newcastle.ac.uk>
2012-03-30 09:37:16 +0100

    Some very basic user permissions
     * User must be logged in to edit/view exams/questions.
     * Users cannot delete exams/questions unless they are the author.
     * Cannot view content unless they are the author (this should be
       replaced with some kind of read-only view of the object).

--- editor/urls.py
+++ editor/urls.py
@@ -40,13 +40,13 @@ urlpatterns = patterns('',
                        
     url(r'^exam/search/$', ExamSearchView.as_view(), name='exam_search'),
     
-    url(r'^exam/(?P<pk>\d+)/(?P<slug>[\w-]+)/$', ExamUpdateView.as_view(),
+    url(r'^exam/(?P<pk>\d+)/(?P<slug>[\w-]+)/$', login_required(ExamUpdateView.as_view()),
         name='exam_edit'),
                        
     url(r'^exam/(?P<pk>\d+)/(?P<slug>[\w-]+)/copy/$',ExamCopyView.as_view(), name='exam_copy',),
                        
     url(r'^exam/(?P<pk>\d+)/(?P<slug>[\w-]+)/delete/$',
-        ExamDeleteView.as_view(), name='exam_delete'),
+        login_required(ExamDeleteView.as_view()), name='exam_delete'),
     
     url(r'^exam/(?P<pk>\d+)/(?P<slug>[\w-]+)/preview/$',
         ExamPreviewView.as_view(), name='exam_preview'),
@@ -66,12 +66,12 @@ urlpatterns = patterns('',
     url(r'^question/search/$', QuestionSearchView.as_view(), name='question_search',),
     
     url(r'^question/(?P<pk>\d+)/(?P<slug>[\w-]+)/$',
-        QuestionUpdateView.as_view(), name='question_edit'),
+        login_required(QuestionUpdateView.as_view()), name='question_edit'),
                        
     url(r'^question/(?P<pk>\d+)/(?P<slug>[\w-]+)/copy/$',QuestionCopyView.as_view(), name='question_copy',),
                        
     url(r'^question/(?P<pk>\d+)/(?P<slug>[\w-]+)/delete/$',
-        QuestionDeleteView.as_view(), name='question_delete'),
+        login_required(QuestionDeleteView.as_view()), name='question_delete'),
                        
     url(r'^question/(?P<pk>\d+)/(?P<slug>[\w-]+)/preview/$',
         QuestionPreviewView.as_view(), name='question_preview'),

e62275739c6f1a97e47df21775d0d7c248dba590
Christian Perfect <christianperfect@gmail.com>
2012-10-09 14:27:42 +0100

    fix custom javascript function evaluation for IE
    same reason as numbas/Numbas@7451c1b

--- editor/static/js/question/edit.js
+++ editor/static/js/question/edit.js
@@ -323,7 +323,7 @@ $(document).ready(function() {
 					}
 				}
 				function makeJavascriptFunction(fn) {
-					var preamble='(function('+fn.paramNames.join(',')+'){';
+					var preamble='fn.jfn=(function('+fn.paramNames.join(',')+'){';
 					var math = Numbas.math;
 					var util = Numbas.util;
 					var jfn = eval(preamble+fn.definition+'})');

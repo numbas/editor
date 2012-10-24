80567ea0d53202637d95c9c26c2cffe527748603
Christian Perfect <christianperfect@gmail.com>
2012-10-24 18:49:31 +0100

    put try/catch around auth backends
    when either of the auth backend modules isn't installed, just catch the error silently

--- numbas_auth.py
+++ numbas_auth.py
@@ -1,34 +1,39 @@
-from django_auth_ldap.backend import LDAPBackend
-from shibboleth.middleware import ShibbolethRemoteUserMiddleware
-
-class NumbasAuthBackend(LDAPBackend):
-    
-    """Authentication backend overriding LDAPBackend.
-    
-    This could be used to override certain functionality within the LDAP authentication backend.
-    The example here overrides get_or_create_user() to alter the LDAP givenName attribute.
-    
-    To use this backend, edit settings.py and replace the LDAPBackend line in AUTHENTICATION_BACKENDS with
-        numbas_auth.NumbasAuthBackend
+try:
+    from django_auth_ldap.backend import LDAPBackend
+    class NumbasAuthBackend(LDAPBackend):
         
-    """
-    
-    def get_or_create_user(self, username, ldap_user):
-        """Alter the LDAP givenName attribute to the familiar first name in displayName."""
+        """Authentication backend overriding LDAPBackend.
         
-        ldap_user.attrs['givenName'] = [ldap_user.attrs['displayName'][0].split()[0]]
-        return super(NumbasAuthBackend, self).get_or_create_user(username, ldap_user)
+        This could be used to override certain functionality within the LDAP authentication backend.
+        The example here overrides get_or_create_user() to alter the LDAP givenName attribute.
+        
+        To use this backend, edit settings.py and replace the LDAPBackend line in AUTHENTICATION_BACKENDS with
+            numbas_auth.NumbasAuthBackend
+            
+        """
+        
+        def get_or_create_user(self, username, ldap_user):
+            """Alter the LDAP givenName attribute to the familiar first name in displayName."""
+            
+            ldap_user.attrs['givenName'] = [ldap_user.attrs['displayName'][0].split()[0]]
+            return super(NumbasAuthBackend, self).get_or_create_user(username, ldap_user)
+except ImportError:
+    pass
 
+try:
+    from shibboleth.middleware import ShibbolethRemoteUserMiddleware
+    class NumbasShibbolethRemoteUserMiddleware(ShibbolethRemoteUserMiddleware):
+        """Authentication backend overriding ShibbolethRemoteUserMiddleware."""
 
-class NumbasShibbolethRemoteUserMiddleware(ShibbolethRemoteUserMiddleware):
-    """Authentication backend overriding ShibbolethRemoteUserMiddleware."""
+        # If the Shibboleth username header should not be REMOTE_USER, then set
+        # what it should be here.  This is the only user attribute that should be
+        # set here; all others should be set in shib_auth.py
+        header = 'uname'
 
-    # If the Shibboleth username header should not be REMOTE_USER, then set
-    # what it should be here.  This is the only user attribute that should be
-    # set here; all others should be set in shib_auth.py
-    header = 'uname'
+        def make_profile(self, user, shib_meta):
+            """Transform the displayname into a friendly first name."""
+            user.first_name = user.first_name.split()[0]
+            user.save()
+except ImportError:
+    pass
 
-    def make_profile(self, user, shib_meta):
-        """Transform the displayname into a friendly first name."""
-        user.first_name = user.first_name.split()[0]
-        user.save()

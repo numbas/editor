44ee19243e1c0bb3e002b797698bc83dd9fd7bbe
Christian Perfect <christianperfect@gmail.com>
2013-06-13 16:58:30 +0100

    add SITE_TITLE setting
    default is 'Numbas'

--- editor/context_processors.py
+++ editor/context_processors.py
@@ -6,4 +6,5 @@ def global_settings(request):
 		'ALLOW_REGISTRATION': settings.ALLOW_REGISTRATION,
 		'CAN_LOGOUT': settings.CAN_LOGOUT,
 		'CAN_CHANGE_PASSWORD': settings.CAN_CHANGE_PASSWORD,
+		'SITE_TITLE': settings.SITE_TITLE
     }

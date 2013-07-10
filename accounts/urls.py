18bafa8da1825823cde079ff8c3967110e469519
Christian Perfect <christianperfect@gmail.com>
2013-07-10 16:39:51 +0100

    add first login screen directing towards tutorials
    resolves #158

--- accounts/urls.py
+++ accounts/urls.py
@@ -59,35 +59,38 @@ urlpatterns = patterns('',
                        )
 
 if settings.CAN_CHANGE_PASSWORD:
-	urlpatterns += patterns('',
-						   url(r'^profile/change-password$',
-							   login_required(ChangePasswordView.as_view()),
-							   name='change_password'),
-						   )
+    urlpatterns += patterns('',
+                           url(r'^profile/change-password$',
+                               login_required(ChangePasswordView.as_view()),
+                               name='change_password'),
+                           )
 
 if settings.ALLOW_REGISTRATION:
-	urlpatterns += patterns('',
-						   url(r'^activate/complete/$',
+    urlpatterns += patterns('',
+                           url(r'^activate/complete/$',
                                TemplateView.as_view(template_name='registration/activation_complete.html'),
-							   name='registration_activation_complete'),
-						   # Activation keys get matched by \w+ instead of the more specific
-						   # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
-						   # that way it can return a sensible "invalid key" message instead of a
-						   # confusing 404.
-						   url(r'^activate/(?P<activation_key>\w+)/$',
-							   activate,
-							   {'backend': 'accounts.backend.Backend'},
-							   name='registration_activate'),
-						   url(r'^register/$',
-							   register,
-							   {'backend': 'accounts.backend.Backend'},
-							   name='registration_register'),
-						   url(r'^register/complete/$',
-							   TemplateView.as_view(template_name='registration/registration_complete.html'),
-							   name='registration_complete'),
-						   url(r'^register/closed/$',
-							   TemplateView.as_view(template_name='registration/registration_closed.html'),
-							   name='registration_disallowed'),
-						   )
+                               name='registration_activation_complete'),
+                           # Activation keys get matched by \w+ instead of the more specific
+                           # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+                           # that way it can return a sensible "invalid key" message instead of a
+                           # confusing 404.
+                           url(r'^activate/(?P<activation_key>\w+)/$',
+                               activate,
+                               {'backend': 'accounts.backend.Backend'},
+                               name='registration_activate'),
+                           url(r'^register/$',
+                               register,
+                               {'backend': 'accounts.backend.Backend'},
+                               name='registration_register'),
+                           url(r'^register/complete/$',
+                               TemplateView.as_view(template_name='registration/registration_complete.html'),
+                               name='registration_complete'),
+                           url(r'^register/closed/$',
+                               TemplateView.as_view(template_name='registration/registration_closed.html'),
+                               name='registration_disallowed'),
+                           url(r'^register/after-first-login/$',
+                               TemplateView.as_view(template_name='registration/after_first_login.html'),
+                               name='after_first_login'),
+                           )
 
 

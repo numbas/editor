7008162911757b5b68116e333d5784bd7c55555d
Christian Perfect <christianperfect@gmail.com>
2014-09-25 14:52:44 +0100

    add json_filter.py back in
    Even though I used git mv to rename the file, it seems it didn't get added to the last commit.

---
+++ editor/templatetags/json_filter.py
@@ -0,0 +1,14 @@
+from django.core.serializers import serialize
+from django.db.models.query import QuerySet
+import json
+from django.template import Library
+
+register = Library()
+
+def jsonify(object):
+    if isinstance(object, QuerySet):
+        return serialize('json', object)
+    return json.dumps(object)
+
+register.filter('json', jsonify)
+

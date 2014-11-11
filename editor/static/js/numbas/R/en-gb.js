0d1db62ee6903431d9ac439a610876b122aee75e
Christian Perfect <christianperfect@gmail.com>
2014-11-11 15:33:06 +0000

    update Numbas locale file

--- editor/static/js/numbas/R/en-gb.js
+++ editor/static/js/numbas/R/en-gb.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 Newcastle University
+Copyright 2011-14 Newcastle University
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,16 +14,67 @@ Copyright 2011 Newcastle University
    limitations under the License.
 */
 R.registerLocale('en-GB',{
+	'page.loading': "Loading...",
+	'page.saving': "<p>Saving.</p>\n<p>This might take a few seconds.</p>",
+
+	'die.numbas failed': "Numbas has failed",
+	'die.sorry': "Sorry, Numbas has encountered an error which means it can't continue. Below is a description of the error.",
+	'die.error': "Error",
+
+	'exam.exam name': "Exam Name:",
+	'exam.random seed': "Session ID:",
+	'exam.number of questions': "Number of Questions:",
+	'exam.marks available': "Marks Available:",
+	'exam.pass percentage': "Pass Percentage:",
+	'exam.time allowed': "Time Allowed:",
+	'exam.passed': 'Passed',
+	'exam.failed': 'Failed',
+	'exam.review header': "Review: ",
+	'frontpage.start': "Start",
+
+	'suspend.exam suspended': "The Exam has been suspended. Press Resume to continue.",
+	'suspend.you can resume': "You will be able to resume this session the next time you start this activity.",
+	'suspend.resume': "Resume",
+
+	'result.exit': "Exit Exam",
+	'result.print': "Print this results summary",
+	'result.review': "Review",
+	'result.exam summary': "Exam Summary",
+	'result.performance summary': "Performance Summary",
+	'result.exam start': "Exam Start:",
+	'result.exam stop': "Exam Stop:",
+	'result.time spent': "Time Spent:",
+	'result.questions attempted': "Questions Attempted:",
+	'result.score': "Score:",
+	'result.result': "Result:",
+	'result.detailed question breakdown': "Detailed Question Breakdown and Feedback",
+	'result.question number': "Question Number",
+	'result.question score': "Score",
+	'result.question review title': "Review this question",
+	'result.click a question to review': "Click on a question number to see how your answers were marked and, where available, full solutions.",
+
+	'end.exam has finished': "The exam has finished. You may now close this window.",
+
 	'control.confirm leave': "You haven't finished the exam.",
 	'control.not all questions answered': "You have not completed every question in this exam.",
+	'control.not all questions submitted': "You have made changes to one or more answers but not submitted them. Please check each question has been submitted.",
 	'control.confirm end': "Are you sure you want to end the exam? After you end the exam, you will not be able to change any of your answers.",
 	'control.confirm regen': "Would you like to re-randomise this question? If you click OK, all your answers and marks for the current question will be lost.",
-	'control.confirm reveal': "Would you like to reveal the answer to this question? Any marks you have received so far will be removed and you will not be able to answer this question later.",
+	'control.confirm reveal': "Would you like to reveal the answer to this question? Any marks you have received so far will be locked and you will not be able to answer this question later.",
+	'control.proceed anyway': "Proceed anyway?",
 	'control.regen': "Try another question like this one",
 	'control.submit answer': "Submit answer",
 	'control.submit all parts': "Submit all parts",
 	'control.submit again': "Submit again",
 	'control.submit': "Submit",
+	'control.previous': "Previous",
+	'control.next': "Next",
+	'control.advice': "Advice",
+	'control.reveal': "Reveal answers",
+	'control.total': "Total",
+	'control.pause': "Pause",
+	'control.end exam': "End Exam",
+	'control.back to results': "Go back to results",
 
 	'display.part.jme.error making maths': "Error making maths display",
 	
@@ -34,7 +85,7 @@ R.registerLocale('en-GB',{
 	'feedback.you were awarded': "You were awarded %s.",
 	'feedback.taken away': "%s %s taken away.",
 
-	'jme.tokenise.invalid': "Invalid expression: %s",
+	'jme.tokenise.invalid': "Invalid expression: <code>%s</code>",
 
 	'jme.shunt.not enough arguments': "Not enough arguments for operation %s",
 	'jme.shunt.no left bracket in function': "No matching left bracket in function application or tuple",
@@ -48,6 +99,8 @@ R.registerLocale('en-GB',{
 	'jme.typecheck.function not defined': "Operation '%s' is not defined. Did you mean <br/><code>%s*(...)</code>?",
 	'jme.typecheck.op not defined': "Operation '%s' is not defined.",
 	'jme.typecheck.no right type definition': "No definition of '%s' of correct type found.",
+	'jme.typecheck.no right type unbound name': "Variable <code>%s</code> is not defined.",
+	'jme.typecheck.map not on enumerable': "<code>map</code> operation must work over a list or a range, not %s",
 
 	'jme.evaluate.undefined variable': "Variable %s is undefined",
 
@@ -64,10 +117,13 @@ R.registerLocale('en-GB',{
 	'jme.user javascript.error': "Error in user-defined javascript function <code>%s</code><br/>%s",
 	'jme.user javascript.error': "User-defined javascript function <code>%s</code> didn't return anything",
 
-	'jme.variables.variable not defined': "Variable %s is not defined.",
+	'jme.variables.error making function': "Error making function <code>%s</code>: %s",
+	'jme.variables.syntax error in function definition': "Syntax error in function definition",
+	'jme.variables.variable not defined': "Variable <code>%s</code> is not defined.",
 	'jme.variables.empty definition': "Definition of variable %s is empty.",
-	'jme.variables.circular reference': "Circular variable reference in question %s %s",
+	'jme.variables.circular reference': "Circular variable reference in definition of %s",
 	'jme.variables.error computing dependency': "Error computing referenced variable <code>%s</code>",
+	'jme.variables.error evaluating variable': "Error evaluating variable %s: %s",
 
 	'jme.display.unknown token type': "Can't texify token type %s",
 	'jme.display.collectRuleset.no sets': 'No sets given to collectRuleset!',
@@ -91,6 +147,10 @@ R.registerLocale('en-GB',{
 	'vectormath.dot.matrix too big': "Can't calculate dot product of a matrix which isn't $1 \\times N$ or $N \\times 1$.",
 	'vectormath.cross.matrix too big': "Can't calculate cross product of a matrix which isn't $1 \\times N$ or $N \\times 1$.",
 
+	'part.with steps answer prompt': 'Answer: ',
+	
+	'part.script.error': "Error in part %s custom script %s: %s",
+
 	'part.marking.steps no matter': "Because you received full marks for the part, your answers to the steps aren't counted.",
 	'part.marking.steps change single': "You were awarded <strong>%s</strong> mark for your answers to the steps",
 	'part.marking.steps change plural': "You were awarded <strong>%s</strong> marks for your answers to the steps",
@@ -104,6 +164,7 @@ R.registerLocale('en-GB',{
 	'part.marking.nothing entered': "You did not enter an answer.",
 	'part.marking.incorrect': "Your answer is incorrect.",
 	'part.marking.correct': "Your answer is correct.",
+	'part.correct answer': "Correct answer:",
 
 	'part.missing type attribute': "Missing part type attribute",
 	'part.unknown type': "Unrecognised part type %s",
@@ -113,7 +174,7 @@ R.registerLocale('en-GB',{
 	'part.jme.answer missing': "Correct answer for a JME part is missing (%s)",
 	'part.jme.answer too long': "Your answer is too long.",
 	'part.jme.answer too short': "Your answer is too short.",
-	'part.jme.answer invalid': "Your answer is not a valid mathematical expression.<br/>%s",
+	'part.jme.answer invalid': "Your answer is not a valid mathematical expression.<br/>%s.",
 	'part.jme.marking.correct': "Your answer is numerically correct.",
 	'part.jme.must-have bits': '<span class="monospace">%s</span>',
 	'part.jme.must-have one': "Your answer must contain: %s",
@@ -121,38 +182,83 @@ R.registerLocale('en-GB',{
 	'part.jme.not-allowed bits': '<span class="monospace">%s</span>',
 	'part.jme.not-allowed one': "Your answer must not contain: %s",
 	'part.jme.not-allowed several': "Your answer must not contain any of: %s",
+	'part.jme.unexpected variable name': "Your answer was interpreted to use the unexpected variable name <code>%s</code>.",
+	'part.jme.unexpected variable name suggestion': "Your answer was interpreted to use the unexpected variable name <code>%s</code>. Did you mean <code>%s</code>?",
 
 	'part.patternmatch.display answer missing': "Display answer is missing from a Pattern Match part (%s)",
 	'part.patternmatch.correct except case': "Your answer is correct, except for the case.",
 
 	'part.numberentry.correct except decimal': "Your answer is within the allowed range, but decimal numbers are not allowed.",
 	'part.numberentry.answer invalid': "You did not enter a valid number.",
+	'part.numberentry.answer not integer': "Your answer is invalid. You must enter a whole number, not a decimal.",
+	'part.numberentry.answer not integer or decimal': "Your answer is invalid. You must enter an integer or a decimal.",
 
 	'part.mcq.choices missing': "Definition of choices is missing from a Multiple Response part (%s)",
 	'part.mcq.matrix not a number': "Part %s marking matrix cell %s,%s does not evaluate to a number",
 	'part.mcq.wrong number of choices': "You selected the wrong number of choices.",
 	'part.mcq.no choices selected': "No choices selected.",
 	'part.mcq.matrix not a list': "Marking matrix for a Multiple Response part, defined by JME expression, is not a list but it should be.",
+	'part.mcq.matrix wrong type': "Element of invalid type '%s' used in marking matrix.",
+	'part.mcq.matrix mix of numbers and lists': "Mix of numbers and lists used in marking matrix.",
+	'part.mcq.matrix wrong size': "Marking matrix is the wrong size.",
 	'part.mcq.correct choice': "You chose the correct answer.",
 
 	'part.gapfill.feedback header': '<strong>Gap %i</strong>',
 	
+	'question.loaded name mismatch': "Can't resume this attempt - the package has changed since the last session.",
+	'question.error': "Question %i: %s",
+	'question.preamble.error': "Error in preamble: %s",
+	'question.preamble.syntax error': "Syntax error in preamble",
+	'question.unsupported part type': "Unsupported part type",
 	'question.header': "Question %i",
 	'question.substituting': "Error substituting content: <br/>%s<br/>%s",
+	'question.submit part': "Submit part",
+	'question.show steps': "Show steps",
+	'question.show steps penalty': "You will lose <strong>%s</strong> %s.",
+	'question.show steps no penalty': "Your score will not be affected.",
+	'question.show steps already penalised': "You have already shown steps. You can show them again with no further penalty.",
+	'question.hide steps': "Hide steps",
+	'question.hide steps no penalty': "Your score will not be affected.",
+	'question.advice': "Advice",
 	'question.no such part': "Can't find part %s",
 	'question.can not submit': "Can not submit answer - check for errors.",
 	'question.answer submitted': "Answer submitted",
+	'question.unsubmitted changes.several parts': "You have made changes to your answers but not submitted them. Please check your answers to each part and then press the <strong>Submit all parts</strong> button.",
+	'question.unsubmitted changes.one part': "You have made a change to your answer but not submitted it. Please check your answer and then press the <strong>Submit answer</strong> button.",
 
+	'question.score feedback.show': 'Show feedback',
+	'question.score feedback.hide': 'Hide feedback',
 	'question.score feedback.answered total actual': "Score: %(score)/%(marks)",
 	'question.score feedback.answered total': "%(marksString). Answered.",
 	'question.score feedback.answered actual': "Score: %(scoreString)",
 	'question.score feedback.answered': "Answered.",
+	'question.score feedback.unanswered': "Unanswered.",
 	'question.score feedback.unanswered total': "%(marksString).",
+	'question.score feedback.correct': 'Your answer is correct',
+	'question.score feedback.partial': 'Your answer is partially correct',
+	'question.score feedback.wrong': 'Your answer is incorrect',
+
+	'question.selector.unsubmitted changes': "Unsubmitted changes.",
 	
 	'timing.no accumulator': "no timing accumulator %s",
 	'timing.time remaining': "Time remaining: %s",
 	
 	'xml.could not load': "Couldn't load an XML document: %s",
 	'xml.property not number': "Property %s should be a number, but isn't (%s), in node %s",
-	'xml.property not boolean': "Property %s should be a boolean, but isn't (%s), in node %s"
+	'xml.property not boolean': "Property %s should be a boolean, but isn't (%s), in node %s",
+	'xml.error in variable definition': "Error in definition of variable <code>%s</code>",
+
+	'scorm.error initialising': "Error initialising SCORM protocol: %s",
+	'scorm.failed save': "<p>The request to save data to the server failed. Press <b>OK</b> to try again.</p>\n<p>If you get this message repeatedly, check your internet connection or use a different computer. Your previously submitted answers have been successfully saved and will be restored if you resume this session on a different computer.</p>\n<p>If this message appears persistently and you can't save <em>any</em> answers, please email <a href=\"mailto:numbas@ncl.ac.uk\">numbas@ncl.ac.uk</a>.</p>",
+	'scorm.no exam suspend data': "Failed to resume: no exam suspend data.",
+	'scorm.error loading suspend data': "Error loading suspend data: %s",
+	'scorm.error loading question': "Error loading question %s: %s",
+	'scorm.no question suspend data': "No question suspend data",
+	'scorm.error loading part': "Error loading part %s: %s",
+	'scorm.no part suspend data': "No part suspend data",
+
+	'mark': 'mark',
+	'marks': 'marks',
+	'was': 'was',
+	'were': 'were'
 });

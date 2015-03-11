ba37ad20c6cdafdd29f056308c7707d9aa7e79c5
Christian Perfect <christianperfect@gmail.com>
2015-03-11 14:29:24 +0000

    just one migration to add stamps
    getting ready to use in production

--- editor/migrations/0005_stampofapproval.py
+++ editor/migrations/0005_stampofapproval.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('object_id', models.PositiveIntegerField()),
-                ('status', models.CharField(max_length=20, choices=[(b'ok', b'Ready to use'), (b'problem', b'Not ready to use')])),
+                ('status', models.CharField(max_length=20, choices=[(b'ok', b'Ready to use'), (b'dontuse', b'Should not be used'), (b'problem', b'Has some problems'), (b'broken', b"Doesn't work")])),
                 ('date', models.DateTimeField(default=datetime.datetime(1970, 1, 1, 0, 0), auto_now_add=True)),
                 ('object_content_type', models.ForeignKey(to='contenttypes.ContentType')),
                 ('user', models.ForeignKey(related_name='stamps', to=settings.AUTH_USER_MODEL)),
@@ -28,5 +28,17 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='exam',
+            name='current_stamp',
+            field=models.ForeignKey(to='editor.StampOfApproval', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='current_stamp',
+            field=models.ForeignKey(to='editor.StampOfApproval', null=True),
+            preserve_default=True,
         ),
     ]

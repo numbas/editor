e07c6d3e0a986c59da5bb6606d58405139633e52
Christian Perfect <christianperfect@gmail.com>
2015-10-13 11:18:46 +0100

    admin icons are a bit more reusable
    The CSS for admin icons (preview,copy, etc.) used to assume they were inside links and had no siblings, which is no good.
    This adds a new template tag, admin_icon, which lets you stick an icon in anywhere, and name it by its function instead of the icon it uses.

--- editor/templatetags/icon.py
+++ editor/templatetags/icon.py
@@ -6,3 +6,14 @@ register = Library()
 @register.inclusion_tag('iconic-svg.html')
 def icon(icon,size=15):
     return {'icon': icon, 'size': size}
+
+admin_icons = {
+    'preview': 'media-play',
+    'copy': 'plus',
+    'basket': 'basket',
+    'delete': 'x',
+}
+
+@register.inclusion_tag('admin-icon.html')
+def admin_icon(kind):
+    return {'kind': kind, 'icon': admin_icons[kind]}

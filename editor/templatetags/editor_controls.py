6537cdf59685ce5a2edbac01f420b352d37116ff
Christian Perfect <christianperfect@gmail.com>
2016-03-22 13:26:54 +0000

    fix context in editor control template tags

--- editor/templatetags/editor_controls.py
+++ editor/templatetags/editor_controls.py
@@ -5,7 +5,7 @@ register = Library()
 
 @register.inclusion_tag('editor-controls/property.html', takes_context=True)
 def property(context,property,label,*args,**kwargs):
-    context = context.update({
+    context.update({
         'property': property,
         'label': label,
         'dynamic_label': kwargs.get('dynamic_label',False),
@@ -21,7 +21,7 @@ def property(context,property,label,*args,**kwargs):
 
 @register.inclusion_tag('editor-controls/percentproperty.html', takes_context=True)
 def percentproperty(context,property,label,*args,**kwargs):
-    context = context.update({
+    context.update({
         'property': property,
         'label': label,
         'help_url': kwargs.get('help_url'),
@@ -32,7 +32,7 @@ def percentproperty(context,property,label,*args,**kwargs):
 
 @register.inclusion_tag('editor-controls/selectproperty.html', takes_context=True)
 def selectproperty(context,property,label,*args,**kwargs):
-    context = context.update({
+    context.update({
         'property': property,
         'label': label,
         'options': kwargs.get('options'),
@@ -45,7 +45,7 @@ def selectproperty(context,property,label,*args,**kwargs):
 
 @register.inclusion_tag('editor-controls/booleanproperty.html', takes_context=True)
 def booleanproperty(context,property,label,*args,**kwargs):
-    context = context.update({
+    context.update({
         'property': property,
         'label': label,
         'help_url': kwargs.get('help_url'),
@@ -56,7 +56,7 @@ def booleanproperty(context,property,label,*args,**kwargs):
 
 @register.inclusion_tag('editor-controls/exam_event.html',takes_context=True)
 def exam_event(context,property,name,*args,**kwargs):
-    context = context.update({
+    context.update({
         'property': property,
         'name': name,
         'help_url': kwargs.get('help_url'),

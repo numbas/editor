764a430d2c1741e336ff390527d8eeca0dfb9e80
Christian Perfect <christianperfect@gmail.com>
2016-04-13 13:42:50 +0100

    move "make a copy" and "delete" buttons to panel on settings tab

--- editor/models.py
+++ editor/models.py
@@ -83,10 +83,10 @@ class ControlledObject(object):
         return (user.is_superuser) or (self.owner==user) or (self.has_access(user,accept_levels))
 
     def can_be_copied_by(self,user):
-        if not self.licence:
-            return False
         if user.is_superuser or self.owner==user or self.has_access(user,('edit',)):
             return True
+        elif not self.licence:
+            return False
         else:
             return self.licence.can_reuse and self.licence.can_modify
 

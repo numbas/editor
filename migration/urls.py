99a757d64a54ab128310a9fa434bd61f02f1d73b
Christian Perfect <christianperfect@gmail.com>
2016-05-31 13:06:44 +0100

    login_required on migration tool

--- migration/urls.py
+++ migration/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$',views.IndexView.as_view(),name='migrate_index'),
-    url(r'^items$',views.MigrateItemsView.as_view(),name='migrate_items'),
+    url(r'^$',login_required(views.IndexView.as_view()),name='migrate_index'),
+    url(r'^items$',login_required(views.MigrateItemsView.as_view()),name='migrate_items'),
 )

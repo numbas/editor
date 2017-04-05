0ca27b0b234c3c0a0101447c82a1e1a9f29bb819
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-04-05 09:42:34 +0100

    mathjax url is a site option
    and the default is now cdnjs. This is because cdn.mathjax.org is
    shutting down: https://www.mathjax.org/cdn-shutting-down/

--- editor/context_processors.py
+++ editor/context_processors.py
@@ -7,7 +7,8 @@ def global_settings(request):
         'ALLOW_REGISTRATION': settings.ALLOW_REGISTRATION,
         'CAN_LOGOUT': settings.CAN_LOGOUT,
         'CAN_CHANGE_PASSWORD': settings.CAN_CHANGE_PASSWORD,
-        'SITE_TITLE': settings.SITE_TITLE
+        'SITE_TITLE': settings.SITE_TITLE,
+        'MATHJAX_URL': settings.MATHJAX_URL
     }
 
 def site_root(request):

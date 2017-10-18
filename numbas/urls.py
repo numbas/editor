41a6b0febf487528eb341708831988f99f99206e
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-10-18 11:57:52 +0100

    don't die if debug toolbar isn't installed

--- numbas/urls.py
+++ numbas/urls.py
@@ -32,7 +32,10 @@ urlpatterns += patterns('',
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ImportError:
+        pass

7c95836fa57138af056b7373427b8d91de7d3827
Christian Perfect <christianperfect@gmail.com>
2018-04-19 13:46:41 +0100

    update Numbas runtime

--- editor/static/js/numbas/numbas-runtime.js
+++ editor/static/js/numbas/numbas-runtime.js
@@ -754,7 +754,7 @@ var jme = Numbas.jme = /** @lends Numbas.jme */ {
     /** Does this expression behave randomly?
      *  True if it contains any instances of functions or operations, defined in the given scope, which could behave randomly.
      *
-     *  @param {JME} expr
+     *  @param {Numbas.jme.tree} expr
      *  @param {Numbas.jme.Scope} scope
      *  @returns {Boolean}
      */
@@ -787,6 +787,33 @@ var jme = Numbas.jme = /** @lends Numbas.jme */ {
             default:
                 return false;
         }
+    },
+
+    /** Is this a monomial - a single term of the form x^n or m*x^n, where m and n are numbers?
+     * @param {Numbas.jme.tree}
+     * @returns {Object} the degree of the monomial
+     */
+    isMonomial: function(tree) {
+        function unwrapUnaryMinus(tree) {
+            while(jme.isOp(tree.tok,'-u')) {
+                tree = tree.args[0];
+            }
+            return tree;
+        }
+        tree = unwrapUnaryMinus(tree);
+        if(jme.isOp(tree.tok,'*')) {
+            if(unwrapUnaryMinus(tree.args[0]).tok.type!='number') {
+                return false;
+            }
+            tree = tree.args[1];
+        }
+        if(tree.tok.type=='name') {
+            return {base:tree, degree:1};
+        }
+        if(jme.isOp(tree.tok,'^') && tree.args[0].tok.type=='name' && unwrapUnaryMinus(tree.args[1]).tok.type=='number') {
+            return {base:tree.args[0], degree:tree.args[1].tok.value};
+        }
+        return false;
     }
 };
 
@@ -1355,6 +1382,7 @@ Scope.prototype = /** @lends Numbas.jme.Scope.prototype */ {
             for(var name in scope.functions) {
                 add(name,scope.functions[name])
             }
+            scope = scope.parent;
         }
         return out;
     },
@@ -2361,7 +2389,17 @@ var resultsEqual = jme.resultsEqual = function(r1,r2,checkingFunction,checkingAc
         return util.eq(r1,r2);
     }
 };
-jme.varsUsed = function(tree) {
+
+/** List names of variables used in `tree`, obtained by depth-first search.
+ *
+ * Differs from {@link Numbas.jme.findvars} by including duplicates, and ignoring {@link Numbas.jme.findvarsOps}.
+ * 
+ * @memberof Numbas.jme
+ * @method
+ * @param {Numbas.jme.tree} tree
+ * @returns {String[]}
+ */
+var varsUsed = jme.varsUsed = function(tree) {
     switch(tree.tok.type) {
         case 'name':
             return [tree.tok.name];
@@ -2376,15 +2414,27 @@ jme.varsUsed = function(tree) {
             return [];
     }
 };
-/*
- * compare vars used lexically, then longest goes first if one is a prefix of the other
- * then by data type
- * then by function name
- * otherwise return 0
+/** Compare two trees.
  *
- * @returns -1 if a is less, 0 if equal, 1 if a is more
+ * * Compare lists of variables lexically using {@link Numbas.jme.varsUsed}; longest goes first if one is a prefix of the other
+ * * then monomials before anything else
+ * * then by data type
+ * * then by function name
+ * * otherwise return 0
+ *
+ * @memberof Numbas.jme
+ * @method
+ * @param {Numbas.jme.tree} a
+ * @param {Numbas.jme.tree} b
+ * @returns {Number} -1 if `a` should appear to the left of `b`, 0 if equal, 1 if `a` should appear to the right of `b`
  */
-jme.compareTrees = function(a,b) {
+var compareTrees = jme.compareTrees = function(a,b) {
+    while(jme.isOp(a.tok,'-u')) {
+        a = a.args[0];
+    }
+    while(jme.isOp(b.tok,'-u')) {
+        b = b.args[0];
+    }
     var va = jme.varsUsed(a);
     var vb = jme.varsUsed(b);
     for(var i=0;i<va.length;i++) {
@@ -2398,6 +2448,23 @@ jme.compareTrees = function(a,b) {
     if(vb.length>va.length) {
         return 1;
     }
+
+    var ma = jme.isMonomial(a);
+    var mb = jme.isMonomial(b);
+    var isma = ma!==false;
+    var ismb = mb!==false;
+    if(isma!=ismb) {
+        return isma ? -1 : 1;
+    }
+    if(isma && ismb && !(a.tok.type=='name' && b.tok.type=='name')) {
+        var d = jme.compareTrees(ma.base,mb.base);
+        if(d==0) {
+            return ma.degree<mb.degree ? 1 : ma.degree>mb.degree ? -1 : 0;
+        } else {
+            return d;
+        }
+    }
+
     if(a.tok.type!=b.tok.type) {
         var order = ['op','function'];
         var oa = order.indexOf(a.tok.type);
@@ -2928,7 +2995,6 @@ newBuiltin('togivenprecision', [TString,TString,TNum,TBool], TBool, math.toGiven
 newBuiltin('withintolerance',[TNum,TNum,TNum],TBool, math.withinTolerance);
 newBuiltin('countdp',[TString],TNum, function(s) { return math.countDP(util.cleanNumber(s)); });
 newBuiltin('countsigfigs',[TString],TNum, function(s) { return math.countSigFigs(util.cleanNumber(s)); });
-newBuiltin('rationalapproximation',[TNum,TNum],TList,math.rationalApproximation,{unwrapValues:true});
 newBuiltin('isnan',[TNum],TBool,function(n) {
     return isNaN(n);
 });
@@ -4170,7 +4236,7 @@ var texOps = jme.display.texOps = {
                 } else if (right.tok.type=='number' && (right.tok.value==Math.PI || right.tok.value==Math.E || right.tok.value.complex) && left.tok.type=='number' && !(left.tok.value.complex)) {
                     use_symbol = false
                 //number times a power of i
-                } else if (jme.isOp(right.tok,'^') && right.args[0].tok.type=='number' && math.eq(right.args[0].tok.value,math.complex(0,1)) && left.tok.type=='number')	{
+                } else if (jme.isOp(right.tok,'^') && right.args[0].tok.type=='number' && math.eq(right.args[0].tok.value,math.complex(0,1)) && left.tok.type=='number') {
                     use_symbol = false;
                 // times sign when LHS or RHS is a factorial
                 } else if((left.tok.type=='function' && left.tok.name=='fact') || (right.tok.type=='function' && right.tok.name=='fact')) {
@@ -5724,7 +5790,7 @@ var matchAllTree = jme.rules.matchAllTree = function(ruleTree,exprTree,doCommute
     }
     return matches;
 }
-/** Match expresison against a pattern. Wrapper for {@link Numbas.jme.rules.matchTree}
+/** Match expression against a pattern. Wrapper for {@link Numbas.jme.rules.matchTree}
  *
  * @memberof Numbas.jme.rules
  * @method
@@ -6009,12 +6075,14 @@ var simplificationRules = jme.rules.simplificationRules = {
     cancelFactors: [
         // x*y or rest*x*y
         ['(?;rest*(?;x)^(?;n)) * (?;y)^(?;m)',['n isa "number"','m isa "number"','canonical_compare(x,y)=0'],'rest*x^(n+m)'],
-        ['(?;rest*(?;x)*(?;n)) * ?;y',['n isa "number"','canonical_compare(x,y)=0'],'rest*x^eval(n+1)'],
+        ['(?;rest*(?;x)^(?;n)) * ?;y',['n isa "number"','canonical_compare(x,y)=0'],'rest*x^eval(n+1)'],
         ['(?;rest*?;x) * (?;y)^(?;n)',['n isa "number"','canonical_compare(x,y)=0'],'rest*x^eval(n+1)'],
         ['(?;rest*?;x) * ?;y',['canonical_compare(x,y)=0'],'rest*x^2'],
         ['(?;x)^(?;n)*(?;y)^(?;m)',['n isa "number"','m isa "number"','canonical_compare(x,y)=0'],'x^eval(n+m)'],
         ['(?;x)^(?;n)*?;y',['n isa "number"','canonical_compare(x,y)=0'],'x^eval(n+1)'],
+        ['(?;x)^(-?;n)*?;y',['n isa "number"','canonical_compare(x,y)=0'],'x^eval(-n+1)'],
         ['?;x*(?;y)^(?;n)',['n isa "number"','canonical_compare(x,y)=0'],'x^eval(n+1)'],
+        ['?;x*(?;y)^(-?;n)',['n isa "number"','canonical_compare(x,y)=0'],'x^eval(-n+1)'],
         ['?;x*?;y',['canonical_compare(x,y)=0'],'x^2'],
         // x/y or rest*x/y
         ['(?;rest*(?;x)^(?;n)) / ((?;y)^(?;m))',['n isa "number"','m isa "number"','canonical_compare(x,y)=0'],'rest*x^eval(n-m)'],
@@ -6033,7 +6101,7 @@ var simplificationRules = jme.rules.simplificationRules = {
         ['(?;rest/((?;x)^(?;n))) / ?;y',['n isa "number"','canonical_compare(x,y)=0'],'rest/(x^eval(n+1))'],
         ['(?;rest/?;x) / ((?;y)^(?;n))',['n isa "number"','canonical_compare(x,y)=0'],'rest/(x^eval(1+n))'],
         ['(?;rest/?;x) / ?;y',['canonical_compare(x,y)=0'],'rest/(x^2)'],
-        ['(?;rest/?;x) / ?;y',['canonical_compare(x,y)=0'],'rest/(x^0)']
+        ['(?;rest/?;x) * ?;y',['canonical_compare(x,y)=0'],'rest/(x^0)']
     ],
     collectLikeFractions: [
         ['?;a/?;b+?;c/?;d',['canonical_compare(b,d)=0'],'(a+c)/b']
@@ -6046,12 +6114,18 @@ var canonicalOrderRules = [
     ['-?;x+?;y',['canonical_compare(x,y)=1'],'y-x'],
     ['-?;x-?;y',['canonical_compare(x,y)=1'],'(-y)-x'],
     ['(?;x+?;y)+?;z',['canonical_compare(y,z)=1'],'(x+z)+y'],
+    ['(?;x+?;y)-?;z',['canonical_compare(y,z)=1'],'(x-z)+y'],
+    ['(?;x-?;y)+?;z',['canonical_compare(y,z)=1'],'(x+z)-y'],
+    ['(?;x-?;y)-?;z',['canonical_compare(y,z)=1'],'(x-z)-y'],
     ['?;x*?;y',['canonical_compare(x,y)=-1'],'y*x'],
-    ['(?;x*?;y)*?;z',['canonical_compare(y,z)=-1'],'(x*z)*y']
+    ['(?;x*?;y)*?;z',['canonical_compare(y,z)=-1'],'(x*z)*y'],
+    ['?;x*(?;y*?;z)',['canonical_compare(x,y)=-1'],'y*(x*z)'],
 ]
 var expandBracketsRules = [
     ['(?;x+?;y)*?;z',[],'x*z+y*z'],
-    ['?;x*(?;y+?;z)',[],'x*y+x*z']
+    ['?;x*(?;y+?;z)',[],'x*y+x*z'],
+    ['(?;x-?;y)*?;z',[],'x*z-y*z'],
+    ['?;x*(?;y-?;z)',[],'x*y-x*z']
 ]
 /** Compile an array of rules (in the form `[pattern,conditions[],result]` to {@link Numbas.jme.rules.Rule} objects
  * @param {Array} rules
@@ -13408,7 +13482,7 @@ Numbas.queueScript('answer-widgets',['knockout','util','jme','jme-display'],func
             this.classes['answer-widget-'+this.widget] = true;
         },
         template: '\
-        <span data-bind="css: classes, component: {name: \'answer-widget-\'+widget, params: {answerJSON: answerJSON, part: part, disable: disable, options: widget_options}}"></span>\
+        <span data-bind="if: widget"><span data-bind="css: classes, component: {name: \'answer-widget-\'+widget, params: {answerJSON: answerJSON, part: part, disable: disable, options: widget_options}}"></span></span>\
         '
     });
     ko.components.register('answer-widget-string', {

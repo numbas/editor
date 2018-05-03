32df132ac0db355318cce73b9eca6e1594aebeac
Christian Perfect <christianperfect@gmail.com>
2018-05-03 10:57:06 +0100

    fix raw_include when DEBUG=False
    I didn't realise staticfiles_storage gives you a bytes object, not a
    string.

--- editor/templatetags/raw_include.py
+++ editor/templatetags/raw_include.py
@@ -18,7 +18,8 @@ def raw_include(path):
         if absolute_path is None:
             raise Exception("raw_include: couldn't find file {}".format(path))
         f = codecs.open(absolute_path, 'r', 'utf-8')
+        content = f.read()
     else:
         f = staticfiles_storage.open(path)
-    content = f.read()
+        content = f.read().decode('utf-8')
     return mark_safe(content)

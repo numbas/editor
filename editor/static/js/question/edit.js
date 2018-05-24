b7d348def6af7244a754a91dd266235b478dc676
Christian Perfect <christianperfect@gmail.com>
2018-05-24 10:40:21 +0100

    fix Variable.definition
    the code introduced in #412 only applies to 'anything' variables, but
    was above the switch statement so ran for all variable types.

--- editor/static/js/question/edit.js
+++ editor/static/js/question/edit.js
@@ -1394,14 +1394,14 @@ $(document).ready(function() {
                 var treeToJME = Numbas.jme.display.treeToJME;
                 var wrapValue = Numbas.jme.wrapValue;
                 try {
-                    var tokens = Numbas.jme.tokenise(val.definition());
-                    if(tokens.length > 2) {
-                        if(Numbas.jme.isName(tokens[0],this.name()) && Numbas.jme.isOp(tokens[1],'=')) {
-                            throw("You don't need to include <code>"+this.name()+" =</code> at the start of your definition.");
-                        }
-                    }
                     switch(templateType) {
                     case 'anything':
+                        var tokens = Numbas.jme.tokenise(val.definition());
+                        if(tokens.length > 2) {
+                            if(Numbas.jme.isName(tokens[0],this.name()) && Numbas.jme.isOp(tokens[1],'=')) {
+                                throw("You don't need to include <code>"+this.name()+" =</code> at the start of your definition.");
+                            }
+                        }
                         return val.definition()+'';
                     case 'number':
                         var n = parseFloat(val.value());

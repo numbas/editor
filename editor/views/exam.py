e9e5fcfcec26bc3c3c5135664ef586c10ee2c00e
Christian Perfect <christianperfect@gmail.com>
2018-09-04 09:27:05 +0100

    import User in editor.views.exam

--- editor/views/exam.py
+++ editor/views/exam.py
@@ -5,6 +5,7 @@ import operator
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.urls import reverse, resolve, Resolver404
 from django.db import transaction

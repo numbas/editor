e1a19e9da2c33b2ff7004b7ec6db8c2cef3cc4d6
Christian Lawson-Perfect <christianperfect@gmail.com>
2019-05-09 10:54:15 +0100

    update Numbas runtime

--- editor/static/js/numbas/numbas-runtime.js
+++ editor/static/js/numbas/numbas-runtime.js
@@ -18168,6 +18168,19 @@ var toObject = function (o) {
     return Object(o);
 };
 });
+
+if (!Object.entries) {
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    
+    return resArray;
+  };
+}
+
 });
 
 Numbas.queueScript('es6-promise',[],function(module) {

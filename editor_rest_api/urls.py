c29bb343f4e01054aad61b9b0de47dfd931b4f1c
Christian Lawson-Perfect <christianperfect@gmail.com>
2020-01-13 10:12:38 +0000

    fix url routing for REST API
    In the latest version, the `base_name` argument changed to `basename`.

--- editor_rest_api/urls.py
+++ editor_rest_api/urls.py
@@ -6,10 +6,10 @@ from . import viewsets
 router = routers.DefaultRouter()
 router.register(r'projects',viewsets.ProjectViewSet)
 router.register(r'users', viewsets.UserViewSet)
-router.register(r'exams', viewsets.ExamViewSet,base_name='exam')
-router.register(r'questions', viewsets.QuestionViewSet,base_name='question')
+router.register(r'exams', viewsets.ExamViewSet,basename='exam')
+router.register(r'questions', viewsets.QuestionViewSet,basename='question')
 router.register(r'resources',viewsets.ResourceViewSet)
-router.register(r'available-exams', viewsets.AvailableExamsViewSet,base_name='available-exams')
+router.register(r'available-exams', viewsets.AvailableExamsViewSet,basename='available-exams')
 
 urls = [
     url(r'^', include(router.urls)),

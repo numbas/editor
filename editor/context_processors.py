84609ab4a29dc93b7c16f371e31e0409f434ed5b
Christian Lawson-Perfect <christianperfect@gmail.com>
2022-07-28 12:26:54 +0100

    add CUSTOM_COLORS setting
    I want to have the navbar a different colour on the beta editor.
    
    This adds a setting CUSTOM_COLORS to settings.py, which should have a
    key 'brand-color' whose value is a CSS colour.

--- editor/context_processors.py
+++ editor/context_processors.py
@@ -12,6 +12,11 @@ def global_settings(request):
         'MATHJAX_URL': settings.MATHJAX_URL,
         'NUMBAS_LOCALES': settings.GLOBAL_SETTINGS['NUMBAS_LOCALES'],
         'URL_PREFIX': getattr(settings,'URL_PREFIX','/'),
+        'CUSTOM_COLORS': getattr(settings, 'CUSTOM_COLORS',
+            {
+                'brand-color': '#acdeff',
+            }
+        ),
     }
 
 def site_root_url(request):

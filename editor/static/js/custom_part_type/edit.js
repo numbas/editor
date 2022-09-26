53fc8d465dbfd303d85338b7bf60e96038c6143a
Christian Lawson-Perfect <christianperfect@gmail.com>
2022-09-26 15:36:09 +0100

    wait for extensions to load before loading the rest of the data
    fixes #727

--- editor/static/js/custom_part_type/edit.js
+++ editor/static/js/custom_part_type/edit.js
@@ -406,7 +406,7 @@ $(document).ready(function() {
                 script_promises.push(promise);
                 document.head.appendChild(script);
             });
-            Promise.all(script_promises).then(function() {
+            return Promise.all(script_promises).then(function() {
                 Numbas.activateExtension(ext.location);
                 ext.loaded(true);
             }).catch(function(err) {
@@ -629,16 +629,20 @@ $(document).ready(function() {
             },this));
         },
 
-        load: function(data) {
+        load: async function(data) {
             var pt = this;
             tryLoad(data,['name','short_name','description','help_url','published'],this);
 
+            var extensionPromises = [];
             if('extensions' in data) {
                 this.extensions().map(function(e) {
-                    if(data.extensions.indexOf(e.location)>=0)
+                    if(data.extensions.indexOf(e.location)>=0) {
                         e.used(true);
+                        extensionPromises.push(e.load());
+                    }
                 });
             }
+            await Promise.all(extensionPromises);
             if('settings' in data && data.settings.forEach) {
                 data.settings.forEach(function(sd) {
                     var setting = new Setting(pt, sd);

6e31355b3783a93cb7adc850188d75393f0523bf
Janet Cheung <jcworkandschool@gmail.com>
2023-07-13 14:34:15 +0100

    Default choice box shows up as empty
    fixes #777

--- editor/static/js/question/part_types.js
+++ editor/static/js/question/part_types.js
@@ -828,7 +828,7 @@ part_types.models = [
 
             model.addChoice = function() {
                 var c = {
-                    content: Editor.contentObservable('Choice '+(model.choices().length+1)),
+                    content: Editor.contentObservable(''),
                     marks: ko.observable(0),
                     distractor: Editor.contentObservable(''),
                     answers: ko.observableArray([])
@@ -1037,7 +1037,7 @@ part_types.models = [
 
             model.addChoice = function() {
                 var c = {
-                    content: Editor.contentObservable('Choice '+(model.choices().length+1)),
+                    content: Editor.contentObservable(''),
                     marks: ko.observable(0),
                     distractor: Editor.contentObservable('')
                 };

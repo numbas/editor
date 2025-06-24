4c3bce128d16c31877a223aa4627326549bb48f8
Christian Lawson-Perfect <christianperfect@gmail.com>
2025-06-24 16:29:18 +0100

    set the Numbas locale in the editor

--- editor/static/js/numbas/editor-extras.js
+++ editor/static/js/numbas/editor-extras.js
@@ -3,7 +3,9 @@ Numbas.queueScript('base',[],function() {
 Numbas.queueScript('display',[],function() {
 });
 
-Numbas.queueScript('editor-extras',['jme-display'],function() {
+Numbas.queueScript('editor-extras',['jme-display','localisation'],function() {
+    Numbas.locale.set_preferred_locale('en-GB');
+    Numbas.locale.init();
     Numbas.jme.display.texOps.subvar = function(thing,texArgs) {
         return '\\class{jme-subvar}{\\left\\{'+texArgs[0]+'\\right\\}}';
     };

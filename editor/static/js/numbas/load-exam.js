a7a56e8c630a0de1560679cfede7505da653d42e
Christian Lawson-Perfect <christianperfect@gmail.com>
2025-06-24 11:37:27 +0100

    add JS and CSS to extensions in the generic runtime
    When a preview is displayed, the directories of all extensions are
    walked to find .js and .css files for the runtime to load.
    Hopefully this is fast enough that we can avoid caching this.
    
    This also changes the `should_compile` logic to look at the runtime
    directory in the compiler, and recompile if that's changed.
    
    The preferred locale is passed to the Numbas runtime.

---
+++ editor/static/js/numbas/load-exam.js
@@ -0,0 +1,28 @@
+Numbas.queueScript('go',['start-exam', 'display'], async function() {
+    const params = new URLSearchParams(location.search);
+
+    const locale = params.get('locale') || 'en-GB';
+
+    Numbas.locale.set_preferred_locale(locale);
+
+    const options = {exam_url: params.get('source_url')};
+
+    const exam_data = await Numbas.load_exam(options);
+
+    const extension_data = JSON.parse(params.get('extensions'));
+
+    for(let extension of exam_data.extensions) {
+        const data = extension_data[extension];
+        for(let js of data.javascripts) {
+            const script = document.createElement('script');
+            script.src = `${data.root}/${js}`;
+            document.head.appendChild(script);
+        }
+        for(let css of data.stylesheets) {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = `${data.root}/${css}`;
+            document.head.appendChild(link);
+        }
+    }
+});

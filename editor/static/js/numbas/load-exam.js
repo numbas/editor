3de64da642b6d095c3c3a8892dec26c87618e2a8
Christian Lawson-Perfect <christianperfect@gmail.com>
2025-07-08 09:41:37 +0100

    Numbas.load_exam is called by the <numbas-exam> element

--- editor/static/js/numbas/load-exam.js
+++ editor/static/js/numbas/load-exam.js
@@ -5,31 +5,10 @@ Numbas.queueScript('go',['start-exam', 'display'], async function() {
 
     Numbas.locale.set_preferred_locale(locale);
 
+    const container = document.getElementById('with-stylesheet');
+
     const root_element = document.createElement('numbas-exam');
-    document.getElementById('with-stylesheet').append(root_element);
-    console.log('appended');
-
-    const options = {
-        exam_url: params.get('source_url'),
-        element: root_element
-    };
-
-    const exam_data = await Numbas.load_exam(options);
-
-    const extension_data = JSON.parse(params.get('extensions'));
-
-    for(let extension of exam_data.extensions) {
-        const data = extension_data[extension];
-        for(let js of data.javascripts) {
-            const script = document.createElement('script');
-            script.src = `${data.root}/${js}`;
-            document.head.appendChild(script);
-        }
-        for(let css of data.stylesheets) {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = `${data.root}/${css}`;
-            document.head.appendChild(link);
-        }
-    }
+    root_element.setAttribute('source_url', params.get('source_url'));
+    root_element.setAttribute('extensions', params.get('extensions'));
+    container.append(root_element);
 });

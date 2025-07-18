8c3c106f99d476e999c19c07200cca4a6d5cf277
Christian Lawson-Perfect <christianperfect@gmail.com>
2025-07-18 10:46:34 +0100

    load-exam: use a <script> tag for the extension data

--- editor/static/js/numbas/numbas-runtime.js
+++ editor/static/js/numbas/numbas-runtime.js
@@ -231,6 +231,8 @@ Numbas.addExtension = function(name, deps, callback) {
     });
 }
 
+Numbas.extension_url_root = {};
+
 /**
  * Get the URL of a standalone file from an extension.
  *
@@ -239,7 +241,8 @@ Numbas.addExtension = function(name, deps, callback) {
  * @returns {string}
  */
 Numbas.getStandaloneFileURL = function(extension, path) {
-    return 'extensions/' + extension + '/standalone_scripts/' + path;
+    const root = Numbas.extension_url_root[extension] || `extensions/${extension}`;
+    return root + '/standalone_scripts/' + path;
 }
 
 /**
@@ -28725,6 +28728,10 @@ Numbas.queueScript('display-util', ['math'], function() {
      * @param {Element} element
      */
     function force_focus(element) {
+        if(!Numbas.display.control_focus) {
+            return;
+        }
+
         const ot = element.tabIndex;
         element.tabIndex = 0;
         element.focus();

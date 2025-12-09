1950d8edd602638e06b289bf883ef6d89d1f82f1
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2025-12-09 10:08:20 +0000

    \var and \simplify only wrap the TeX in braces if it starts with a - or + character.
    I hadn't documented what motivated the braces.
    If you have `e^\var{x}`, then when `x` is negative, the number needs to be wrapped in braces so the minus sign and the digits are both rendered in superscript.
    
    But when substituting something like a `<` relation into TeX, curly braces stop it getting the right spacing.
    
    So the fudge is to only wrap in curly braces if the generated TeX starts with a - or + character.
    The other option would be to make the author remember to put curly braces, which is easy to miss.

--- editor/static/js/numbas/mathjax_custom/numbas-mathjax.js
+++ editor/static/js/numbas/mathjax_custom/numbas-mathjax.js
@@ -3,30 +3,45 @@
  */
 (function() {
 
-    var NumbasMap = new MathJax._.input.tex.TokenMap.CommandMap(
-        'numbasMap', 
+    /** 
+     * Wrap TeX code in braces if it starts with a `-` character.
+     * This is necessary for cases like `e^\var{-1}`, which should be rendered as `e^{-1}` instead of `e^-1`.
+     *
+     * @param {TeX} tex
+     * @returns {TeX}
+     */
+    function maybe_wrap_tex(tex) {
+        if(tex.match(/^[\-+]/)) {
+           // tex = '{'+tex+'}';
+        }
+        return tex;
+    }
+
+    new MathJax._.input.tex.TokenMap.CommandMap(
+        'numbasMap',
 
         {
             var: ['numbasVar', 'var'],
             simplify: ['numbasSimplify', 'simplify']
-        }, 
+        },
 
-        { 
+        {
             numbasVar: function mmlToken(parser, name, type) {
                 const {jme} = Numbas;
 
+                let expr;
                 try {
                     const settings_string = parser.GetBrackets(name); // The optional argument to the command, in square brackets.
 
                     const settings = {};
-                    if(settings_string!==undefined) {
+                    if(settings_string !== undefined) {
                         settings_string.split(/\s*,\s*/g).forEach(function(v) {
                             var setting = jme.normaliseRulesetName(v.trim());
                             settings[setting] = true;
                         });
                     }
 
-                    const expr = parser.GetArgument(name);
+                    expr = parser.GetArgument(name);
 
                     const {scope} = parser.configuration.packageData.get('numbas');
 
@@ -34,7 +49,7 @@
 
                     if(scope.showSubstitutions) {
                         const tok = jme.evaluate(expr, scope);
-                        tex = '{' + jme.display.texify({tok}, settings, scope) + '}';
+                        tex = maybe_wrap_tex(jme.display.texify({tok}, settings, scope));
                     } else {
                         const res = Editor.texJMEBit(expr, settings, null, scope);
                         tex = `\\class{jme-var}{\\left\\{${res.tex || res.message}\\right\\}}`;
@@ -44,7 +59,7 @@
 
                     parser.Push(mml);
                 } catch(e) {
-                    if(e.retry) {
+                    if(e.retry instanceof Promise) {
                         throw e;
                     }
 
@@ -57,20 +72,21 @@
             numbasSimplify: function mmlToken(parser, name, type) {
                 const {jme} = Numbas;
 
+                let expr;
                 try {
                     let ruleset = parser.GetBrackets(name); // The optional argument to the command, in square brackets.
                     if(ruleset === undefined) {
                         ruleset = 'all';
                     }
 
-                    const expr = parser.GetArgument(name);
+                    expr = parser.GetArgument(name);
 
                     const {scope} = parser.configuration.packageData.get('numbas');
 
                     let tex;
                     if(scope.showSubstitutions) {
                         const subbed_tree = Numbas.jme.display.subvars(expr, scope);
-                        tex = '{' + Numbas.jme.display.treeToLaTeX(subbed_tree, ruleset, scope) + '}';
+                        tex = maybe_wrap_tex(Numbas.jme.display.treeToLaTeX(subbed_tree, ruleset, scope));
                     } else {
                         const res = Editor.texJMEBit(expr, ruleset, null, scope);
                         tex = `\\class{jme-simplify}{\\left\\{${res.tex || res.message}\\right\\}}`;
@@ -80,10 +96,9 @@
 
                     parser.Push(mml);
                 } catch(e) {
-                    if(e.retry) {
+                    if(e.retry instanceof Promise) {
                         throw e;
                     }
-
                     console.error(e);
                     throw(new Numbas.Error('mathjax.math processing error',{message:e.message,expression:expr}));
                 }
@@ -94,17 +109,17 @@
 
     function saveJMEScope(arg) {
         const scope = Numbas.display_util.find_jme_scope(arg.math.start.node);
-        arg.data.packageData.set('numbas',{scope});
+        arg.data.packageData.set('numbas', {scope});
     }
 
 
-    var NumbasConfiguration = MathJax._.input.tex.Configuration.Configuration.create('numbas', {
+    MathJax._.input.tex.Configuration.Configuration.create('numbas', {
         handler: {
             macro: ['numbasMap']
         },
         preprocessors: [
             [saveJMEScope, 1]
-        ],  
+        ],
     });
 
 })();
